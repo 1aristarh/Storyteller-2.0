435923739e798fa547592fc32f597d42160c94af
Petar Atanasov <firedragon2004bg@gmail.com>
2023-04-21 19:39:32 +0300

    Add project files.

---
+++ Storyteller 2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Storyteller_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
