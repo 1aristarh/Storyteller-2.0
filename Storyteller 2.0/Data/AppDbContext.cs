55c68fa6085b324668e6faa2983aa900e96bd3bf
Petar Atanasov <firedragon2004bg@gmail.com>
2023-04-25 23:32:03 +0300

    Before Identity

--- Storyteller 2.0/Data/AppDbContext.cs
+++ Storyteller 2.0/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Storyteller_2._0.Models;
 
 namespace Storyteller_2._0.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
